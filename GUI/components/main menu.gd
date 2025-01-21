b405c340e303e395ccb3cce0234d8529707a3967
Kevin Andre Engell 74263421+Kevengg@users.noreply.github.com <kevinandre.engell@gmail.com>
2025-01-21 10:10:54 +0100

    added way for game to load save from menu

--- GUI/components/main menu.gd
+++ GUI/components/main menu.gd
@@ -1,8 +1,17 @@
 extends Control
 
-var save = preload("res://data/saveData.json").data
+var helper = Game.helpers
+
+@export var saveData: JSON = null
+@export var items: Node = null
+
+var save = preload("res://data/saveData.json").data[0]
 
 func _ready():
+
+	helper.checkItem(saveData, get_tree(), (scene_file_path + " - " + "no save data selected"))
+	helper.checkItem(items, get_tree(), (scene_file_path + " - " + "no place to put items"))
+
 	# set project version
 	if ProjectSettings.get_setting("application/config/version") == "":
 		$"Control/version".text = "DEVELOPMENT"
@@ -22,5 +31,10 @@ func _on_exit_pressed():
 
 
 func _on_continue_pressed():
+	get_tree().root.set_meta("saveData", save)
+	var map = preload("res://map/map.tscn").instantiate() as Node3D
+	map.saveData = save
+	map.items = items
+	get_tree().root.add_child(map)
 	
 	queue_free()
--- tools/game.gd
+++ tools/game.gd
@@ -2,22 +2,20 @@ extends Node
 class_name Game
 
 
-
 class json:
 	extends Node
-	static func updateJsonFile(json:JSON):
+	static func updateJsonFile(json: JSON):
 		var file = FileAccess.open(json.resource_path, FileAccess.WRITE)
-		file.store_string(JSON.stringify(json.data, "\t") )
+		file.store_string(JSON.stringify(json.data, "\t"))
 		
 	
-	
-	static func parseVector4i(string:String) :
+	static func parseVector4i(string: String):
 		
 		var regex = RegEx.new()
 		regex.compile(r"\(([0-9]*?),([0-9]*?),([0-9]*?),([0-9]*?)\)")
 		var r = regex.search(string)
 		if (!!r):
-			return Vector4i(int(r.get_string(1)),int(r.get_string(2)),int(r.get_string(3)),int(r.get_string(4)))
+			return Vector4i(int(r.get_string(1)), int(r.get_string(2)), int(r.get_string(3)), int(r.get_string(4)))
 		return null
 	
 	## parse a vector as string of x length to a libeary of x length FIXME not implemented
@@ -25,5 +23,9 @@ class json:
 		#pass
 	#
 	
-	
+class helpers:
 
+	static func checkItem(item, get_tree, errorText, errorCode = 1):
+		if !item:
+			printerr(errorText)
+			get_tree.quit(errorCode)
